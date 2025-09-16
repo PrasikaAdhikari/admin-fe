44f59503965803884c4ca65508f5fce4d684958b
Prashikaa <prasika.adhikari11@gmail.com>
2025-09-16 10:35:01 +1000

    Fully Configured packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { Button } from "react-bootstrap";
 import "./App.css";
+import { ToastContainer, toast } from "react-toastify";
+import { RiHome4Fill } from "react-icons/ri";
 
 function App() {
-  return <h1> Hello world</h1>;
+  toast.success("vOILA!");
+  return (
+    <>
+      <Button>
+        {" "}
+        Click here
+        <RiHome4Fill />
+      </Button>
+      <h1> Hello world</h1>
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
