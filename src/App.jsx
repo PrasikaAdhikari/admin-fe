01bcb755790e59648de8e6dfbdf6abd629ee2518
Deep Ghotane <ghotane.deep@gmail.com>
2025-09-18 00:26:02 +1000

    login successsful

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,14 @@ import "./App.css";
 import { ToastContainer, toast } from "react-toastify";
 import { RiHome4Fill } from "react-icons/ri";
 import Login from "./pages/Login";
+import { Provider } from "react-redux";
 
 function App() {
   toast.success("vOILA!");
   return (
     <>
-      {/* <Button>
-        {" "}
-        Click here
-        <RiHome4Fill />
-      </Button>
-      <h1> Hello world</h1> */}
       <Login />
+
       <ToastContainer />
     </>
   );
