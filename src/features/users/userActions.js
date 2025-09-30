ffca048621475504c24d2179c2aa9d815b14fcbf
Manish <100024795+manishhh08@users.noreply.github.com>
2025-09-30 11:21:51 +1000

    Update userActions.js

--- src/features/users/userActions.js
+++ src/features/users/userActions.js
@@ -72,6 +72,4 @@ export const updateUserDetailAction = (form) => async (dispatch) => {
   }
   return { status: data.status, message: data.message };
 };
-  toast[data.status](data.message);
-};
-
+ 
