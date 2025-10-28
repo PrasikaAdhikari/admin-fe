647f0a36213ab4deaf0b98ff40aeea97af9101aa
Prashikaa <prasika.adhikari11@gmail.com>
2025-10-28 13:57:15 +1100

    Search bar functionality for the product table complete

--- src/pages/products/ProductLanding.jsx
+++ src/pages/products/ProductLanding.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import ProductTable from "../../components/products/ProductTable";
 import { CgShoppingBag } from "react-icons/cg";
 import { Container } from "react-bootstrap";
+import HeaderSearch from "../../components/layout/HeaderSearch";
 
 const ProductLanding = () => {
   const dispatch = useDispatch();
@@ -18,8 +19,15 @@ const ProductLanding = () => {
     setAllProducts(products);
   }, [products]);
 
+  // Callback for HeaderSearch
+  const handleSearchChange = (filteredProducts) => {
+    // If search input is empty, show all products
+    setAllProducts(filteredProducts.length > 0 ? filteredProducts : products);
+  };
+
   return (
     <Container fluid className="p-4 p-md-5">
+      <HeaderSearch onSearchChange={handleSearchChange}></HeaderSearch>
       <div className="d-flex flex-wrap align-items-center justify-content-between gap-3 mb-4">
         <div className="d-flex align-items-center gap-3">
           <span className="rounded-circle bg-primary bg-opacity-10 p-3">
